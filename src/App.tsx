d3dcaae3f3f46b422dac254bf1149f6eb7694413
Arman Dezfuli <armandezfuli@proton.me>
2025-03-14 12:49:21 +0330

    start project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import UserList from "./components/UserList"
+
+function App() {
+    return (
+        <div>
+            <h1>My App</h1>
+            <p>Welcome to my app!</p>
+            <UserList />
+        </div>
+    )
+}
+
+export default App
